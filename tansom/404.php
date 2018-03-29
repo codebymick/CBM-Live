c4fbd2744c91b2c2c606746e45f15e0264fd85c1
codebymick <codebymick@gmail.com>
2018-03-29 09:16:38 +0200

    coming soon message

---
+++ tansom/404.php
@@ -0,0 +1,19 @@
+<?php
+get_header();
+ $content = explode("<!--more-->", $post->post_content);?>
+
+<div id="links">
+  <div class="container center" style="height: 50vh;">
+    <p style="height:60px!important;"></p>
+    <h1></br></h1>
+    <h1>Coming soon...</h1>
+    <p>This site is under construction.</p>
+    <!--<h1>404 ERROR</h1>
+    <p>what you are looking for does not exist.</p>-->
+
+
+  </div>
+<!-- main content END-->
+</div>
+
+<?php get_footer(); ?>
